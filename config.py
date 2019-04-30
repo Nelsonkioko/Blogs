e29f6e669d1bf11e656f0cdce4ccd1903f194211
NelsonKioko <kiokonelson2@gmail.com>
2019-04-30 13:05:50 +0300

    fix bug

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ class Config:
   MAIL_USE_TLS = True
   MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
   MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-  SUBJECT_PREFIX= 'blogs website'
-  SENDER_EMAIL= 'kiokonelson2@gmail.com'
+  # SUBJECT_PREFIX= 'blogs website'
+  # SENDER_EMAIL= 'kiokonelson2@gmail.com'
 
 class ProdConfig(Config):
   pass
@@ -27,7 +27,7 @@ class DevConfig(Config):
   DEBUG = True
 
 config_options = {
-  # 'development': DevConfig,
+  'development': DevConfig,
   'production': ProdConfig,
   'test': TestConfig
 }
